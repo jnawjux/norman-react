7ae1d741c62fc2fc21fee95bfbcf47d67170b996
John Naujoks <johnnaujoks@gmail.com>
2020-08-21 01:56:55 -0700

    Add mailchimp import, working on testing API access

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import mailchimp from "@mailchimp/mailchimp_marketing";
 
-function App() {
-  return (
-    <div className="app-layout ex4">
-      <header className="section">
-        <i class="em em-speech_balloon" aria-role="presentation" aria-label="SPEECH BALLOON"></i>
-        <p className="title">Norman</p>
-      </header>
-      <main className="section">Main</main>
-      <footer className="section">put your feet up</footer>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-layout ex4">
+        <header className="section">
+          <i className="em em-speech_balloon" aria-role="presentation" aria-label="SPEECH BALLOON"></i>
+          <p className="title">Norman</p>
+        </header>
+        <main className="section">
+          <h1>
+            HELLOOOOOO
+          </h1>
+        </main>
+        <footer className="section">put your feet up</footer>
+      </div>
+    )
+  }
 }
 
 export default App;
