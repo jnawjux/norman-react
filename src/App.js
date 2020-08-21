7b800711abf0ad6f999bedd083a391f420929192
John Naujoks <johnnaujoks@gmail.com>
2020-08-21 00:02:00 -0700

    Basic page template added, some CSS stylying added Emoji CSS to head

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return ( <
-    div className = "App" >
-
-    <
-    /div>
+  return (
+    <div className="app-layout ex4">
+      <header className="section">
+        <i class="em em-speech_balloon" aria-role="presentation" aria-label="SPEECH BALLOON"></i>
+        <p className="title">Norman</p>
+      </header>
+      <main className="section">Main</main>
+      <footer className="section">put your feet up</footer>
+    </div>
   );
 }
 
